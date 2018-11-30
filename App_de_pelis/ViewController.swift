fc5baf580492987bb8d279893662567a4d6c90a6
Elisabeth Hertig <elihertigu54@gmail.com>
2018-11-30 13:04:12 +0100

    Initial Commit

---
+++ App_de_pelis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App_de_pelis
+//
+//  Created by Funciona el wifi ? on 30/11/2018.
+//  Copyright © 2018 Eli Hertig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
